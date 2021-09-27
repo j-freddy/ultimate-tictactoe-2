70674a98821ed1cb82a2d1d10d23447c7b0b0727
Freddy Jiang <jtf.imperial@gmail.com>
2021-09-27 12:47:05 +0100

    Add check win and check if board is filled

--- board/board.py
+++ board/board.py
@@ -6,12 +6,34 @@ class Board:
     self.num_rows = 3
     self.num_cols = 3
     self.cells = []
+    self.winning_cases = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ]
 
   def get_cell_index(self, row, col):
     return row * self.num_cols + col
   
   def get_cell(self, row, col):
     return self.cells[self.get_cell_index(row, col)]
+
+  def check_filled(self):
+    for cell in self.cells:
+      if cell.get_value() == CellValue.Empty:
+        return False
+    
+    return True
+
+  def check_win(self, cell_value):
+    cell_values = [cell.get_value() for cell in self.cells]
+
+    for indices in self.winning_cases:
+      matches = [cell_values[i] == cell_value for i in indices]
+      if set(matches) == {True}:
+        return True
+    
+    return False
 
   def get_winner(self):
     return CellValue.X
